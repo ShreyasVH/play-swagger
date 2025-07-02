6c3ddf441af8894598185030540ad5d1a3d1f8a1
ShreyasVH <shreyas.deve.1@gmail.com>
2025-07-02 12:51:14 +0530

    retry for compile

--- compile.sh
+++ compile.sh
@@ -1,4 +1,11 @@
 rm -rf dist
-sbt clean compile dist
-unzip target/universal/$REPO_NAME-$REPO_VERSION.zip > /dev/null
-mv $REPO_NAME-$REPO_VERSION dist
+
+while true; do
+	sbt clean compile dist
+
+	if [[ -e target/universal/$REPO_NAME-$REPO_VERSION.zip ]]; then
+		unzip target/universal/$REPO_NAME-$REPO_VERSION.zip > /dev/null
+		mv $REPO_NAME-$REPO_VERSION dist
+		break
+	fi
+done
